02850b5b66aff8c109e71d7e8aff0a153b8dc5b5
nan <2324737911@qq.com>
2020-02-04 11:28:59 +0800

    baseclass RandomClass

---
+++ src/baseclass/RandomListNode.java
@@ -0,0 +1,11 @@
+package baseclass;
+
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
