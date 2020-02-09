b8c409558efab41126749bd6220ca95e7f05469e
nan <2324737911@qq.com>
2020-02-09 12:35:14 +0800

    孩子们的游戏(圆圈中最后剩下的数)

---
+++ src/algorithm/LastRemaining_Solution.java
@@ -0,0 +1,69 @@
+package algorithm;
+
+import java.util.ArrayList;
+
+/**
+ * 每年六一儿童节,牛客都会准备一些小礼物去看望孤儿院的小朋友,今年亦是如此。
+ * HF作为牛客的资深元老,自然也准备了一些小游戏。
+ * 其中,有个游戏是这样的:首先,让小朋友们围成一个大圈。然后,他随机指定一个数m,让编号为0的小朋友开始报数。
+ * 每次喊到m-1的那个小朋友要出列唱首歌,然后可以在礼品箱中任意的挑选礼物,并且不再回到圈中,
+ * 从他的下一个小朋友开始,继续0...m-1报数....这样下去....直到剩下最后一个小朋友,可以不用表演,
+ * 并且拿到牛客名贵的“名侦探柯南”典藏版(名额有限哦!!^_^)。
+ * 请你试着想下,哪个小朋友会得到这份礼品呢？(注：小朋友的编号是从0到n-1)
+ *
+ * 如果没有小朋友，请返回-1
+ */
+public class LastRemaining_Solution {
+    /**
+     * 递推式来源：https://blog.csdn.net/crazy__chen/article/details/45115911
+     *                0                            n=1
+     * f(n,m)={
+     *               [f(n-1,m)+m]%n     n>1
+     * @param n n
+     * @param m m
+     * @return s
+     * 16ms
+     * 10000k
+     */
+    public int lastRemaining_Solution(int n, int m) {
+        if(n==0){
+            return -1;
+        }
+        int s = 0;
+        for(int i =2;i<=n;i++){
+            s = (s+m)%i;
+        }
+        return s;
+    }
+
+    /**
+     * 用list爆搜
+     * 47ms
+     * 9884k
+     * @param n
+     * @param m
+     * @return
+     */
+    public int lastRemaining_Solution1(int n, int m) {
+        if(n==0){
+            return -1;
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i =0;i<n;i++){
+            list.add(i);
+        }
+        int count = -1;
+        while (list.size()!=1){
+            for(int j = 0;j<m;j++){
+                count++;
+                if(count==list.size()){
+                    count=0;
+                }
+            }
+            list.remove(count);
+            //cur--的原因，因为新的list中cur指向了下一个元素，为了保证移动m个准确性，所以cur向前移动一位。
+            count--;
+        }
+        return list.get(0);
+    }
+}
