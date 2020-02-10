3d9445b9d66d2bceae4d7074ba09d60058dd676a
nan <2324737911@qq.com>
2020-02-10 12:24:04 +0800

    数组中重复的数字

---
+++ src/algorithm/Duplicate.java
@@ -0,0 +1,95 @@
+package algorithm;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 在一个长度为n的数组里的所有数字都在0到n-1的范围内。
+ * 数组中某些数字是重复的，但不知道有几个数字是重复的。
+ * 也不知道每个数字重复几次。请找出数组中任意一个重复的数字。
+ * 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。
+ */
+public class Duplicate {
+    // Parameters:
+    //    numbers:     an array of integers
+    //    length:      the length of array numbers
+    //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
+    //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
+    //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+
+    /**
+     * 25ms
+     * 9572k
+     * @param numbers
+     * @param length
+     * @param duplication
+     * @return
+     */
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+        if(numbers==null||numbers.length==0){
+            return false;
+        }
+        Arrays.sort(numbers);
+        for(int i =0;i<numbers.length-1;i++){
+            if(numbers[i]==numbers[i+1]){
+                duplication[0] = numbers[i];
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 26ms
+     * 9648k
+     * 通过hashSet
+     * @param numbers numbers
+     * @param length length
+     * @param duplication duplication
+     * @return 0
+     * O(n)
+     */
+    public boolean duplicate1(int numbers[],int length,int [] duplication) {
+        Set<Integer> set = new HashSet<>();
+        for(int i =0;i<length;i++){
+            if(set.contains(numbers[i])){
+                duplication[0] = numbers[i];
+                return true;
+            }else {
+                set.add(numbers[i]);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 数组的长度为 n 且所有数字都在 0 到 n-1 的范围内，我们可以将每次遇到的数进行"归位"，
+     * 当某个数发现自己的"位置"被相同的数占了，则出现重复。
+      * @param numbers
+     * @param length
+     * @param duplication
+     * @return
+     */
+    public boolean duplicate2(int numbers[],int length,int [] duplication) {
+        if(numbers == null || length == 0){
+            return false;
+        }
+        for(int i=0;i<length;i++){
+            while(numbers[i] != i){
+                if(numbers[i] == numbers[numbers[i]]){
+                    duplication[0] = numbers[i];
+                    return true;
+                }
+                // swap
+                int tmp = numbers[i];
+                numbers[i] = numbers[tmp];
+                numbers[tmp] = tmp;
+            }
+        }
+        return false;
+    }
+
+}
