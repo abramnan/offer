608808d24cd0614c827be12c6db62c41b4197f0c
nan <2324737911@qq.com>
2020-02-11 13:12:41 +0800

    删除链表中重复的结点

--- src/algorithm/DeleteDuplication.java
+++ src/algorithm/DeleteDuplication.java
@@ -18,15 +18,20 @@ public class DeleteDuplication {
             return null;
         }
         ListNode p = pHead;
+        // 自己构建辅助头结点
         ListNode head = new ListNode(0);
         ListNode pre = head;
         pre.next = pHead;
         while (p!=null){
             if(p.next!=null&&p.val==p.next.val){
+                // 相同结点一直前进
                 while (p.next!=null&&p.val==p.next.val){
                     p=p.next;
                 }
+                // 退出循环时，cur 指向重复值，也需要删除，而 cur.next 指向第一个不重复的值
+                // cur 继续前进
                 p=p.next;
+                // pre 连接新结点
                 pre.next=p;
             }
             else {
